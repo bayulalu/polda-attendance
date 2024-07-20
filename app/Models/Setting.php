83b891cf84e3e6d0d017353035a6bc0b8572ceaa
bayulalu <bayulalu3@gmail.com>
2024-07-20 15:56:06 +0800

    list setting

---
+++ app/Models/Setting.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Setting extends Model
+{
+
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'day',
+        'email',
+        'time_arrival',
+        'time_gohome'
+    ];
+
+
+}
