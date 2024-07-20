c8c8109deaac093e30aa697cd63e0d4d40169c23
bayulalu <bayulalu3@gmail.com>
2024-07-20 10:41:09 +0800

    list user admin

--- app/Livewire/Admin/User.php
+++ app/Livewire/Admin/User.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\User as ModelsUser;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class User extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.admin.user');
+        $users = ModelsUser::latest()->paginate(20);
+
+        return view('livewire.admin.user', compact('users'));
     }
 }
